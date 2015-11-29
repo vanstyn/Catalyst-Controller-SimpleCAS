9564b80255cbab1f2d6be4bb410d5a17a7a58d2c
Henry Van Styn <vanstyn@intellitree.com>
2015-11-29 10:57:10 -0500

    v0.9982

--- lib/Catalyst/Controller/SimpleCAS.pm
+++ lib/Catalyst/Controller/SimpleCAS.pm
@@ -4,7 +4,7 @@ use warnings;
 
 # ABSTRACT: General-purpose content-addressed storage (CAS) for Catalyst
 
-our $VERSION = '0.9981';
+our $VERSION = '0.9982';
 
 use Moose;
 use Types::Standard qw(:all);
